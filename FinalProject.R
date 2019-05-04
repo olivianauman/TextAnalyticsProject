984dd1debcba8aa2e43e496122ed7822abab29c3
grantmartin1213 <42518439+grantmartin1213@users.noreply.github.com>
2019-05-03 20:49:44 -0500

    Add files via upload

--- FinalProject.R
+++ FinalProject.R
@@ -1,18 +1,42 @@
 ### Text Analytics Final Project
 ### Clarisse Chia, Grant Martin, Olivia Nauman, & Hunter Somers
 
+rm(list=ls())
+
 # libraries
 suppressPackageStartupMessages(library(tm))
 suppressPackageStartupMessages(library(NLP))
 suppressPackageStartupMessages(library(RWeka))
 
 
-# read in dataset & format dataframe
+#read in dataset & format dataframe
 df <- read.csv("yelp_cleveland_restaurants.csv", stringsAsFactors = FALSE)
 names(df) <- c("Review", "Stars", "Date", "Useful", "Funny", "Cool", "Total Votes")
 
+############################################################################################################################
+
+#### Assinging Labels ####
+
+#assigning labels based on count of useful votes
+#df <- mutate(df, Label = ifelse(df$Useful >= 2, "useful", "not_useful"))
+#table(unlist(df$Label))
+
+#assigning labels based on percent of total useful votes
+#df <- mutate(df, Label = ifelse(df$Useful/df$`Total Votes` >= 0.5, "useful", "not_useful"))
+#table(unlist(df$Label))
+
+#assigning labels based on total votes count
+#df <- mutate(df, Label = ifelse(df$`Total Votes` >= 10, "useful", "not_useful"))
+#table(unlist(df$Label))
+
+#assigning labels based on total votes count and a percentage of useful votes to total votes
+df <- mutate(df, Label = ifelse(df$`Total Votes` >= 5 & df$Useful/df$`Total Votes` >= 0.3, "useful", "not_useful"))
+table(unlist(df$Label))
+
+############################################################################################################################
+
 #vector of reviews & ratings
-review <- df$Review
+reviews <- df$Review
 stars <- df$Stars
 
 #vcorpus of reviews
